92da88bf3ba8503cdfeca9b2e4b17446cda6e0cc
NadiaPia <206i417@gmail.com>
2023-12-16 14:18:17 -0800

    Add index.js from scratch in pizza-menu

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+    return <h1>Hello React!</h1>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// React v18
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode><App /></React.StrictMode>) //React.StrictMode the only thing it does it is during development it renders our components twice to find certain bugs and also React will check if we are using updated parts of the React API
+
+// React before v18
+//React.render(<App />) and get rid of the client in import ReactDom from "react-dom";
