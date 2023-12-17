3902970d67bb29d2cf997156c937101311ecf7ea
NadiaPia <206i417@gmail.com>
2023-12-16 22:20:57 -0800

    Add components and style to the pizza-menu

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,13 +1,109 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
-    return <h1>Hello React!</h1>
+  return (
+    <div className="container">
+      <Header />
+      <Menu pizzaData={pizzaData} />
+      <Footer />
+    </div>
+  );
+}
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu(props) {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <ul className="pizzas">
+        {props.pizzaData.map((pizza) => (
+          <Pizza pizza={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </main>
+  );
+}
+function Pizza(props) {
+  return (
+    <li className="pizza">
+      <img src={props.pizza.photoName} alt={props.pizza.name} />
+      <div>
+        <h3>{props.pizza.name}</h3>
+        <p>{props.pizza.ingredients}</p>
+        <span>{props.pizza.price}</span>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} We are currently open
+    </footer>
+  );
 }
 
 // React v18
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>) //React.StrictMode the only thing it does it is during development it renders our components twice to find certain bugs and also React will check if we are using updated parts of the React API
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+); //React.StrictMode the only thing it does it is during development it renders our components twice to find certain bugs and also React will check if we are using updated parts of the React API
 
 // React before v18
 //React.render(<App />) and get rid of the client in import ReactDom from "react-dom";
