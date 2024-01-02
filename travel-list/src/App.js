2c55ff6960d030bbd9365daeb506e9b726dcc377
NadiaPia <206i417@gmail.com>
2024-01-02 15:45:27 -0800

    Add clear list functionality to the travel list

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -19,11 +19,16 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm('Are you sure you want to delete all items?')
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
+      <PackingList items={items} setItems={setItems} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} handleClearList={handleClearList}/>
       <Stats items={items}/>
     </div>
   );
@@ -74,7 +79,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, handleClearList }) {
 
   const [sortBy, setSortBy] = useState("input");
   let sortedItems;
@@ -97,6 +102,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description"> Sort by description</option>          
           <option value="packed"> Sort by packed status</option>
         </select>
+        <button onClick={handleClearList}>Clear list</button>
       </div>
     </div>
   );
