a6eea3abfece12f14e6c220e3ab883dc70835a21
NadiaPia <206i417@gmail.com>
2024-04-30 16:57:54 -0700

    add storing in local storage functionality

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -7,12 +7,18 @@ const average = (arr) =>
 const KEY = "56db0cf5";
 
 export default function App() {
-  const [watched, setWatched] = useState([]);
+  
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  //const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(() => {
+    const storedValue = localStorage.getItem('watched');
+    return JSON.parse(storedValue);
+  });
 
   const tempQuery = "interstellar";
 
@@ -27,12 +33,16 @@ export default function App() {
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
 
-    localStorage.setItem('watched', JSON.stringify([...watched, movie]))
+    // localStorage.setItem('watched', JSON.stringify([...watched, movie]))
   }
 
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(() => {
+    localStorage.setItem('watched', JSON.stringify(watched))
+  }, [watched])
 
   useEffect(() => {
     const controller = new AbortController(); //this is a browser API as well as the fetch function. We need this in the api querry to avoid sending query on every symbol that a user insert during typing the movie name.
